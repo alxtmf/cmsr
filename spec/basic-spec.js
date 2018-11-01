551ed0a544dca2bbc60054c7b90405d460a778e7
altmf <89021632777@mail.ru>
2018-11-02 00:24:00 +0800

    refactor test

--- spec/basic-spec.js
+++ spec/basic-spec.js
@@ -1,39 +1,5 @@
 
-describe("check if test work", function () {
-    it("should test work", function () {
-        var product = 2 * 3;
-        expect(product).toBe(6);
-    });
-});
-
-describe("test db connection", function () {
-    it("connection success", function () {
-        const Sequelize = require('sequelize');
-        const sequelize = new Sequelize('CMSR', 'postgres', 'postgres', {
-            host: 'localhost',
-            dialect: 'postgres',
-            operatorsAliases: false,
-            pool: {
-                max: 5,
-                min: 0,
-                acquire: 30000,
-                idle: 10000
-            }
-        });
-
-        sequelize
-            .authenticate()
-            .then(() => {
-                console.log('Connection has been established successfully.');
-            })
-            .catch(err => {
-                console.error('Unable to connect to the database:', err);
-            });
-    });
-});
-//Fg0PzdflNiISC2ztCsjEGQ==
-
-describe("test select balance slice query", function () {    it("balance slice selected", function () {
+describe("basic tests", function () {
     const Sequelize = require('sequelize');
     const sequelize = new Sequelize('CMSR', 'postgres', 'postgres', {
         host: 'localhost',
@@ -46,37 +12,47 @@ describe("test select balance slice query", function () {    it("balance slice s
             idle: 10000
         }
     });
-
-    sequelize
-        .authenticate()
-        .then(() => {
-            console.log('Connection has been established successfully.');
-        })
-        .catch(err => {
-            console.error('Unable to connect to the database:', err);
-        });
-
     const viberId = 'Fg0PzdflNiISC2ztCsjEGQ==';
-    const dateCreate = new Date(2018, 9, 27, 21, 25, 35, 0);
-    // sequelize.query('SELECT * FROM ' +
-    //     'main.max_slice_rooms_balance_info($viberId, $dateCreate) ',
-    //     { bind: { viberId: viberId, dateCreate: dateCreate }, type: sequelize.QueryTypes.SELECT }
-    // ).then(mrbi => {
-    //     console.log(mrbi)
-    // })
 
-    const SliceRoomBalanceInfoModel = require('../model/balance/SliceRoomBalanceInfo');
-    const SliceRoomBalanceInfo = SliceRoomBalanceInfoModel(sequelize, Sequelize);
+    beforeAll(function() {
+        sequelize.authenticate()
+            .then(() => {
+                console.log('Connection has been established successfully.');
+            })
+            .catch(err => {
+                console.error('Unable to connect to the database:', err);
+            });
+    });
 
-    sequelize.query('SELECT * FROM ' +
-        'main.max_slice_rooms_balance_info($viberId, $dateCreate) ',
-        {   bind: { viberId: viberId, dateCreate: dateCreate },
-            type: sequelize.QueryTypes.SELECT,
-            mapToModel: true,
-            model: SliceRoomBalanceInfo
-        }
-    ).then(mrbi => {
-        console.log(mrbi)
-    })
-});
+    it("balance slice selected not object", function () {
+        const dateCreate = new Date(2018, 9, 27, 21, 25, 35, 0);
+        sequelize.query('SELECT * FROM ' +
+            'main.max_slice_rooms_balance_info($viberId, $dateCreate) ',
+            { bind: { viberId: viberId, dateCreate: dateCreate }, type: sequelize.QueryTypes.SELECT }
+        ).then(mrbi => {
+            console.log(mrbi);
+            expect(mrbi).not.toBeUndefined(mrbi);
+        })
+
+    });
+
+    it("balance slice selected", function () {
+        const dateCreate = new Date(2018, 9, 27, 21, 25, 35, 0);
+        const SliceRoomBalanceInfoModel = require('../model/balance/SliceRoomBalanceInfo');
+        const SliceRoomBalanceInfo = SliceRoomBalanceInfoModel(sequelize, Sequelize);
+
+        sequelize.query('SELECT * FROM ' +
+            'main.max_slice_rooms_balance_info($viberId, $dateCreate) ',
+            {   bind: { viberId: viberId,
+                      dateCreate: dateCreate
+                },
+                type: sequelize.QueryTypes.SELECT,
+                mapToModel: true,
+                model: SliceRoomBalanceInfo
+            }
+        ).then(mrbi => {
+            console.log(mrbi);
+            expect(mrbi).not.toBeUndefined(mrbi);
+        })
+    });
 });
